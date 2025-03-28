e425070cea420650f2aabd46c1a767c5cb443179
Banele Gumata <b.sgumata@gmail.com>
2025-03-28 12:54:19 +0200

    uploading new project

---
+++ src/main/java/peopleSution/DepartmentSolution/DepartmentalSoftwareApplication.java
@@ -0,0 +1,14 @@
+package peopleSution.DepartmentSolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentalSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentalSoftwareApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/peopleSution/DepartmentSolution/DepartmentalSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package peopleSution.DepartmentSolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentalSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
